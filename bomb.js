7e92f11d606d5bf009ffa8c29cf58ae85dcbabdf
Bram De Moor <bram.demoor@gmail.com>
2013-01-26 23:19:29 +0100

    Refactor

--- bomb.js
+++ bomb.js
@@ -1,27 +1,33 @@
 var PALETTE = ["#140c1c", "#442434", "#30346d", "#4e4a4e", "#854c30", "#346524", "#d04648", "#757161", "#597dce", "#d27d2c", "#8595a1", "#6daa2c", "#d2aa99", "#6dc2ca", "#dad45e", "#deeed6"];
 
+function l(path) {
+	var tmp = new Image; 
+	simploader(tmp); 
+	tmp.src = path;
+	return tmp;
+}
+
 $(function() {
-  var image = new Image;   simploader(image);  image.src = "input/complexion2.png";
-  var imgFace = new Image;   simploader(imgFace);  imgFace.src = "input/face.png";
-  var imgPistol = new Image;  simploader(imgPistol);  imgPistol.src = "input/pistol.png";
-  var imgSpark = new Image; simploader(imgSpark); imgSpark.src = "input/spark.png";
-  var imgOcto = new Image; simploader(imgOcto); imgOcto.src = "input/octolegs.png";
+  var imgBlood = l("input/blood.png");
+  var imgComplexion = l("input/complexion2.png");
+  var imgFace = l("input/face.png");
+  var imgPistol = l("input/pistol.png");
+  var imgSpark = l("input/spark.png");
+  var imgOcto = l("input/octolegs.png");
+  var imgWeapon = l("input/crazyweapon.png");
   
-	simploader(function() {
-		var mask = cq(image).grayscaleToMask();			
+	simploader(function() {	
 		cq(32,32)
-		.drawImage(image,0,0)
+		.drawImage(imgComplexion,0,0)
 		.shiftHsl(0.41, null, null)		
 		.drawImage(imgOcto, 0, 16)		
-		.drawImage(imgFace, 6, 5)		
-		
-		.matchPalette(PALETTE)
-		
+		.drawImage(imgFace, 6, 5)				
+		.matchPalette(PALETTE)		
 		.appendTo("#test2");
 		
-		cq(imgPistol)
-		.matchPalette(PALETTE)
-		.appendTo("#test2");	
+		cq(imgPistol).matchPalette(PALETTE).appendTo("#test2");	
+		cq(imgWeapon).matchPalette(PALETTE).appendTo("#test2");	
+		cq(imgBlood).matchPalette(PALETTE).appendTo("#test2");	
 
 		cq(20,20)
 			.beginPath().strokeStyle("#000000").arc(10,10,8,0,Math.PI*2,true).closePath().stroke()										
