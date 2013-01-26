2ab79c25756a283eb5f5976b5f20bf17d76065f1
Bram De Moor <bram.demoor@gmail.com>
2013-01-26 23:39:48 +0100

    Wall drawing

--- bomb.js
+++ bomb.js
@@ -36,6 +36,26 @@ $(function() {
 			.beginPath().fillStyle("#70707A").arc(7,7,2,0,Math.PI*2,true).closePath().fill()						
 			.beginPath().strokeStyle("#565653").arc(7,7,2,0,Math.PI*2,true).closePath().stroke()					
 			.drawImage(imgSpark,14,-1)
+			.matchPalette(PALETTE)
 			.appendTo("#test2");
+			
+		var brickWall = cq(32,32);
+		
+		for(var y=0; y<32; y+=6) {
+			var r = (y/6)%2;
+			var rowOffset = r > 0 ? -4 : 0;
+		
+			for(var x=0; x<32; x+=6) {
+				drawBrick(brickWall,x + rowOffset,y);	
+			}
+		}
+		brickWall.matchPalette(PALETTE).appendTo("#test2");
 	})		
 });
+
+function drawBrick(c, x, y) {
+	return c
+	.fillStyle("#AAAAAA").fillRect(x,y,8,6)
+	.strokeStyle("#000000").strokeRect(x,y,8,6)
+}
+
